ff97a3d226c0b4ac4edda5f967de78ae1d805683
Ergi Laze <ergilaze@gmail.com>
2020-04-22 02:00:36 +0200

    React app structure init

---
+++ src/scenes/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyles from 'theme/global';
+import Routes from 'scenes/App/Routes/Routes';
+
+const App = () => (
+  <>
+      <GlobalStyles />
+      <Routes />
+  </>
+);
+
+export default App;
+
---
+++ src/scenes/App/Routes/Routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import routes from 'routes/routes';
+
+const home = () => (
+    <h1>Change in in scenes/App/Routes/Routes</h1>
+)
+
+const Routes = () => (
+    <Switch>
+        <Route path={routes.Home} component={home} exact />
+    </Switch>
+);
+
+export default Routes;
